7480b9d06416b60d879d566b3a57d57643058dd7
Sidney Zhang <liangyi@me.com>
2023-12-26 16:21:09 +0800

    :up: some drafts

---
+++ pyWorkTools/__init__.py
@@ -0,0 +1,11 @@
+
+
+import pyWorkTools.simdata
+import pyWorkTools.simdate
+import pyWorkTools.simplot
+import pyWorkTools.simreader
+import pyWorkTools.simcolor
+
+
+sum = pyWorkTools.simdata.sum
+add = pyWorkTools.simdata.add
